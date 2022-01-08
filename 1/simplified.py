9c34d82d08f7bf5cd1359abfb2ba6f18f8bb5147
shyamg <shyamg@emiprotechnologies.com>
2022-01-08 09:31:42 +0530

    [Mod] manufacturing.py 1st bug fixed [Add] simplified.py

--- 1/simplified.py
+++ 1/simplified.py
@@ -7,24 +7,20 @@ class Manufacturing:
         # product_name
         # ratio_qty is a dict that should have name of raw material as Key and raw material required to build 1 product as a value
 
-        self.raw_material = raw_material.copy()
+        self.raw_material = raw_material
         self.product_name = product_name
-        self.ratio_qty = ratio_qty.copy()
+        self.ratio_qty = ratio_qty
 
     def produce(self, no_of_products):
         # no_of_products no of product to be produced
 
-        temp = self.raw_material.copy()
-        for name, qty in self.ratio_qty.items():
-            # Loop through items in ratio_qty
-            if self.raw_material[name] >= no_of_products * qty:
-                # Verifies that there is enough qty to produce the product
-                temp[name] -= no_of_products * qty  # deduct the used raw material in the process of producing
-            else:
-                # Error msg if there isn't enough raw material available
-                print("\n\nNot enough raw material available to produce the product, please do the purchase!")
-                return False
-        self.raw_material = temp.copy()
+        if self.raw_material >= no_of_products * self.ratio_qty:
+            # Verifies that there is enough qty to produce the product
+            self.raw_material -= no_of_products * self.ratio_qty  # deduct the used raw material in the process of producing
+        else:
+            # Error msg if there isn't enough raw material available
+            print("\n\nNot enough raw material available to produce the product, please do the purchase!")
+            return False
         self.product_qty += no_of_products  # Increase product count
         print("\n\nProduct Produced Successfully")  # Success msg
 
@@ -34,8 +30,7 @@ class Manufacturing:
 
         print("\n\nRaw Material", "   qty")
         print("________________________")
-        for name, qty in self.raw_material.items():
-            print("{:<18}{:<18}".format(name, qty))
+        print("{:<18}{:<18}".format("Wheels", self.raw_material))
 
     def display_final_product_stock(self):
         # Display Product name using product_name
@@ -48,16 +43,14 @@ class Manufacturing:
     def purchase_raw_material(self, new_raw_material):
         # Purchase new Raw Material
         # Loop through passed dict and add value of each item in respective raw_material item value
-
-        for name, qty in new_raw_material.items():
-            self.raw_material[name] += qty
+        self.raw_material += new_raw_material
         print("Material purchased successfully")
 
 
 man = Manufacturing(
-    {'wheels': 10, 'chain': 5, 'handle': 5, 'frame': 5, 'gear': 5},
+    10,
     'bycycle',
-    {'wheels': 2, 'chain': 1, 'handle': 1, 'frame': 1, 'gear': 2}
+    2
 )
 
 while True:
@@ -70,13 +63,7 @@ while True:
 
     option = int(input("Select and option to Continue:  "))
     if option == 1:
-        man.purchase_raw_material({
-            'wheels': int(input("Wheels:  ")),
-            'chain': int(input("Chain:  ")),
-            'handle': int(input("Handle-Bar:  ")),
-            'frame': int(input("Frame:  ")),
-            'gear': int(input("Gear:  "))})
-
+        man.purchase_raw_material(int(input("Wheels:  ")))
     elif option == 2:
         man.produce(int(input("Enter no of product to be produced:  ")))
     elif option == 3:
