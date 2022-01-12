305d4dd33db0231b25f34bdcf8a0607751061dce
shyamg <shyamg@emiprotechnologies.com>
2022-01-12 19:22:53 +0530

    [Mod] 10/10.py

--- 10/10.py
+++ 10/10.py
@@ -1,8 +1,8 @@
 from functools import reduce
 
-list1 = [[2, 4, 5], [4, 6, 8], [3, 6, 9]]
+find_sum_list = [[2, 4, 5], [4, 6, 8], [3, 6, 9]]
 
-concat_lists = reduce(lambda a, b: a+b, list1)
+concat_lists = reduce(lambda a, b: a+b, find_sum_list)
 filtered_even_number_list = list(filter(lambda item: True if item % 2 else False, concat_lists))
 
 print("Total Sum:  ", sum(concat_lists))
