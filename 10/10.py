0872290d42f90ecdfe7cd35f36648dd6b910fd26
shyamg <shyamg@emiprotechnologies.com>
2022-01-15 11:48:22 +0530

    [Mod] 10/10.py 11/Employee_Leave_Attendance.py comments added to each program

--- 10/10.py
+++ 10/10.py
@@ -2,8 +2,8 @@ from functools import reduce
 
 find_sum_list = [[2, 4, 5], [4, 6, 8], [3, 6, 9]]
 
-concat_lists = reduce(lambda a, b: a+b, find_sum_list)
-filtered_even_number_list = list(filter(lambda item: True if item % 2 else False, concat_lists))
+concat_lists = reduce(lambda a, b: a+b, find_sum_list)  # concat inner lists
+filtered_even_number_list = list(filter(lambda item: True if item % 2 else False, concat_lists))  # filters even number
 
-print("Total Sum:  ", sum(concat_lists))
-print("Even number sum:  ", sum(filtered_even_number_list))
+print("Total Sum:  ", sum(concat_lists))  # sum of all elements
+print("Even number sum:  ", sum(filtered_even_number_list))  # sum of even elements
--- 11/Employee_Leave_Attendance.py
+++ 11/Employee_Leave_Attendance.py
@@ -36,17 +36,19 @@ emp_dict = {
 
 
 def attendance_hours(day):
+    # returns sum of attendances hours of each employee
     return reduce(lambda a, b: a + b, day['hours'])
 
 
 def leave_hours(day):
+    # returns leave hours of each employee
     return day['no_of_hours']
 
 
 def emp_details(emp):
-    employee = emp_dict[emp]
-    total_attendance_hours = sum(list(map(attendance_hours, employee['attendances'])))
-    total_leave_hours = sum(list(map(leave_hours, employee['leaves'])))
+    employee = emp_dict[emp]  # single employee details
+    total_attendance_hours = sum(list(map(attendance_hours, employee['attendances'])))  # sum of all days' attendance hours
+    total_leave_hours = sum(list(map(leave_hours, employee['leaves'])))  # sum of total leave hours
     return {'employee_id': emp,
             'employee_name': employee['name'],
             'total_attendance_hours': total_attendance_hours,
@@ -55,23 +57,27 @@ def emp_details(emp):
 
 
 def filter_days(attendances):
+    # filter attendance record of employee whose attendance record have less than 8 hours of attendance per day
     return list(filter(lambda day: True if sum(day['hours']) < 8 else False, attendances))
 
 
 def get_dates(days):
+    # returns date from each filtered attendance record
     return days['date']
 
 
 def get_hours(days):
+    # return sum of hours from each filtered attendance record
     return sum(days['hours'])
 
 
 def remaining_hours(days):
+    # calculate 8 - total attendance hours from each filtered attendance record
     return 8 - sum(days['hours'])
 
 
 def emp_attendance_record(emp):
-    employee = emp_dict[emp]
+    employee = emp_dict[emp]  # details of single employee
     return {
         emp: {
             'date': list(map(get_dates, filter_days(employee['attendances']))),
@@ -81,7 +87,7 @@ def emp_attendance_record(emp):
     }
 
 
-employee_record = list(map(emp_details, emp_dict))
+employee_record = list(map(emp_details, emp_dict))  # first output format dictionary
 print("\n", employee_record)
-employee_work_hours_record = list(map(emp_attendance_record, emp_dict))
+employee_work_hours_record = list(map(emp_attendance_record, emp_dict))  # second output format dictionary
 print("\n\n", employee_work_hours_record)
